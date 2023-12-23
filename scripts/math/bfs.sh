7ac11292f55a0672bcc6bd9d2624d2ab3487f800
Emek Gözlüklü <ge87fil@mytum.de>
2023-12-23 15:17:03 +0100

    update default script

--- scripts/math/bfs.sh
+++ scripts/math/bfs.sh
@@ -1,10 +1,11 @@
 python run.py \
     --task math \
-    --task_start_index 100 \
-    --task_end_index 102 \
-    --method_generate sample \
+    --task_start_index 1000 \
+    --task_end_index 1010 \
+    --method_generate propose \
     --method_evaluate value \
     --method_select greedy \
-    --n_evaluate_sample 1 \
+    --n_evaluate_sample 3 \
     --n_select_sample 1 \
+    --backend gpt-3.5-turbo \
     ${@}
