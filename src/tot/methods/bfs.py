18d54ed2ebb51667768dcf811aba2c1da9eeb782
Emir Gülboy <emirgulboy@gmail.com>
2023-12-26 12:11:45 +0100

    Gpt usage count control added

--- src/tot/methods/bfs.py
+++ src/tot/methods/bfs.py
@@ -3,46 +3,50 @@ import numpy as np
 from functools import partial
 from tot.models import gpt
 
-def get_value(task, x, y, n_evaluate_sample, cache_value=True):
+def get_value(task, x, y, n_evaluate_sample, cache_value=True,limit=20):
     value_prompt = task.value_prompt_wrap(x, y)
     if cache_value and value_prompt in task.value_cache:
         return task.value_cache[value_prompt]
+    #check_gpt_usage(task, limit)
     value_outputs = gpt(value_prompt, n=n_evaluate_sample, stop=None)
     value = task.value_outputs_unwrap(x, y, value_outputs)
     if cache_value:
         task.value_cache[value_prompt] = value
     return value
 
-def get_values(task, x, ys, n_evaluate_sample, cache_value=True):
+def get_values(task, x, ys, n_evaluate_sample, cache_value=True,limit=20):
     values = []
     local_value_cache = {}
     for y in ys:  # each partial output
         if y in local_value_cache:  # avoid duplicate candidates
             value = 0
         else:    
-            value = get_value(task, x, y, n_evaluate_sample, cache_value=cache_value)
+            value = get_value(task, x, y, n_evaluate_sample, cache_value=cache_value,limit=limit)
             local_value_cache[y] = value
         values.append(value)
     return values
 
-def get_votes(task, x, ys, n_evaluate_sample):
+def get_votes(task, x, ys, n_evaluate_sample,limit):
     vote_prompt = task.vote_prompt_wrap(x, ys)
+    #check_gpt_usage(task, limit)
     vote_outputs = gpt(vote_prompt, n=n_evaluate_sample, stop=None)
     values = task.vote_outputs_unwrap(vote_outputs, len(ys))
     return values
 
-def get_proposals(task, x, y): 
+def get_proposals(task, x, y,limit): 
     propose_prompt = task.propose_prompt_wrap(x, y)
+    check_gpt_usage(task, limit)
     proposals = gpt(propose_prompt, n=1, stop=None)[0].split('\n')
     return [y + _ + '\n' for _ in proposals]
 
-def get_samples(task, x, y, n_generate_sample, prompt_sample, stop):
+def get_samples(task, x, y, n_generate_sample, prompt_sample, stop,limit):
     if prompt_sample == 'standard':
         prompt = task.standard_prompt_wrap(x, y)
     elif prompt_sample == 'cot':
         prompt = task.cot_prompt_wrap(x, y)
     else:
         raise ValueError(f'prompt_sample {prompt_sample} not recognized')
+    check_gpt_usage(task, limit)
     samples = gpt(prompt, n=n_generate_sample, stop=stop)
     return [y + _ for _ in samples]
 
@@ -53,19 +57,20 @@ def solve(args, task, idx, to_print=True):
     x = task.get_input(idx)  # input
     ys = ['']  # current output candidates
     infos = []
+    lim = args.limit
     for step in range(task.steps):
         # generation
         if args.method_generate == 'sample':
-            new_ys = [get_samples(task, x, y, args.n_generate_sample, prompt_sample=args.prompt_sample, stop=task.stops[step]) for y in ys]
+            new_ys = [get_samples(task, x, y, args.n_generate_sample, prompt_sample=args.prompt_sample, stop=task.stops[step],limit=lim) for y in ys]
         elif args.method_generate == 'propose':
-            new_ys = [get_proposals(task, x, y) for y in ys]
+            new_ys = [get_proposals(task, x, y,lim) for y in ys]
         new_ys = list(itertools.chain(*new_ys))
         ids = list(range(len(new_ys)))
         # evaluation
         if args.method_evaluate == 'vote':
-            values = get_votes(task, x, new_ys, args.n_evaluate_sample)
+            values = get_votes(task, x, new_ys, args.n_evaluate_sample,limit=lim)
         elif args.method_evaluate == 'value':
-            values = get_values(task, x, new_ys, args.n_evaluate_sample)
+            values = get_values(task, x, new_ys, args.n_evaluate_sample,limit=lim)
 
         # selection
         if args.method_select == 'sample':
@@ -92,8 +97,18 @@ def solve(args, task, idx, to_print=True):
 
 def naive_solve(args, task, idx, to_print=True):
     global gpt
+    check_gpt_usage(task, args.limit)
     gpt = partial(gpt, model=args.backend, temperature=args.temperature)
     print(gpt)
     x = task.get_input(idx)  # input
     ys = get_samples(task, x, '', args.n_generate_sample, args.prompt_sample, stop=None)
     return ys, {}
+
+def check_gpt_usage(task, limit):
+    if not hasattr(task, "gpt_usage"):
+        return
+    if task.gpt_usage > limit:
+        raise ValueError(f'gpt usage {task.gpt_usage} exceeds 20')
+    else:
+        print(f'gpt usage {task.gpt_usage}')
+        task.gpt_usage += 1
